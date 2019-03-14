3bd1f96e2cce9fb4b38b0c915cf4ca9e46ed4a83
VNKMX <VNKMXX@Gmail.com>
2019-03-14 23:24:20 +0800

    ray.h

--- ToyRayTracing/main.cpp
+++ ToyRayTracing/main.cpp
@@ -3,18 +3,32 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include<stb_image_write.h>
 
-#include "vec3.h"
+#include "ray.h"
+
+vec3 color(const ray& r)
+{
+	vec3 unit_direction = unit_vector(r.direction());
+	float t = 0.5*(unit_direction.y() + 1.0);
+	return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
 
 int main()
 {
 	std::vector<unsigned char> image;
-	int nx = 2000;
-	int ny = 1000;
+	int nx = 1920;
+	int ny = 1080;
+	vec3 lower_left_corner(-2.0, -1.0, -1.0);
+	vec3 horizontal(4.0, 0.0, 0.0);
+	vec3 vertical(0.0, 2.0, 0.0);
+	vec3 origin(0.0, 0.0, 0.0);
 	for (int j = ny - 1; j >= 0; j--)
 	{
 		for (int i = 0; i < nx; i++)
 		{
-			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+			float u = float(i) / float(nx);
+			float v = float(j) / float(ny);
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+			vec3 col = color(r);
 			image.push_back(int(255.99*col.r()));
 			image.push_back(int(255.99*col.g()));
 			image.push_back(int(255.99*col.b()));
