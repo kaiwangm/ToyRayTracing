461a203669c1ad6cbd4b70c3c644712ba3f8ae05
VNKMX <VNKMXX@Gmail.com>
2019-03-15 09:32:42 +0800

    make sphere

--- ToyRayTracing/main.cpp
+++ ToyRayTracing/main.cpp
@@ -4,19 +4,40 @@
 #include<stb_image_write.h>
 
 #include "ray.h"
-
+float hit_sphere(const vec3& center, const float radius, const ray& r)
+{
+	vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0*dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;
+	float discriminant = b * b - 4 * a*c;
+	if (discriminant < 0)
+	{
+		return -1.0;
+	}
+	else
+	{
+		return (-b - sqrt(discriminant)) / (2.0*a);
+	}
+}
 vec3 color(const ray& r)
 {
+	float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
+	if (t > 0.0)
+	{
+		vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
+		return 0.5*vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+	}
 	vec3 unit_direction = unit_vector(r.direction());
-	float t = 0.5*(unit_direction.y() + 1.0);
+	t = 0.5*(unit_direction.y() + 1.0);
 	return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
 int main()
 {
 	std::vector<unsigned char> image;
-	int nx = 1920;
-	int ny = 1080;
+	int nx = 2000;
+	int ny = 1000;
 	vec3 lower_left_corner(-2.0, -1.0, -1.0);
 	vec3 horizontal(4.0, 0.0, 0.0);
 	vec3 vertical(0.0, 2.0, 0.0);
